ce77b9c10593b98d2d278b1f02deda1533b8696e
Rhuwell Samano <20153521+rhuwellsamano@users.noreply.github.com>
2022-07-17 19:07:45 -0400

    everything works - code challenge done

--- src/App.js
+++ src/App.js
@@ -1,22 +1,176 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
 
 function App() {
+  const [files, setFiles] = useState([]);
+  const [isEmailFilter, setIsEmailFilter] = useState(false);
+  const [isPhoneFilter, setIsPhoneFilter] = useState(false);
+  const [isFormDisabled, setIsFormDisabled] = useState(true);
+
+  // submit button disables
+  useEffect(() => {
+    if (!!files && (isEmailFilter || isPhoneFilter)) {
+      setIsFormDisabled(false);
+    } else {
+      setIsFormDisabled(true);
+    }
+  }, [files, isEmailFilter, isPhoneFilter]);
+
+  // takes file as a string and converts to array of arrays
+  const convertFileToMatrix = (fileString) => {
+    let arr = fileString.split("\n");
+    return arr.map((rowString) => rowString.split(","));
+  };
+
+  // logic for filtering out duplicate rows
+
+  const removeDupes = (fileArray) => {
+    const dict = {
+      emails: {},
+      phones: {},
+    };
+    return fileArray.filter((rowArr) => {
+      let keepRow = true;
+      let emailEntry = rowArr[2];
+      let phoneEntry = rowArr[3];
+      if (isEmailFilter && emailEntry !== "" && emailEntry in dict.emails) {
+        keepRow = false;
+      }
+      if (isPhoneFilter && phoneEntry !== "" && phoneEntry in dict.phones) {
+        keepRow = false;
+      }
+      if (keepRow) {
+        if (isEmailFilter) dict.emails[emailEntry] = true;
+        if (isPhoneFilter) dict.phones[phoneEntry] = true;
+      }
+      return keepRow;
+    });
+  };
+
+  // converts fileMatrix to CSV file for output
+  const convertToCsv = (filteredFile) => {
+    let newCsvFile = "data:text/csv;charset=utf-8,";
+    newCsvFile += filteredFile.map((row) => row.join(",")).join("\n");
+    return encodeURI(newCsvFile);
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const fileAsString = await files[0].text();
+
+    const fileAsMatrix = convertFileToMatrix(fileAsString);
+    const filteredFile = removeDupes(fileAsMatrix);
+    const newFileAsCsv = convertToCsv(filteredFile);
+    window.open(newFileAsCsv);
+    // alert("File successfully filtered!");
+  };
+
+  const handleFile = (e) => {
+    setFiles(e.target.files);
+  };
+
+  const handleEmailFilter = (e) => {
+    setIsEmailFilter(!isEmailFilter);
+  };
+
+  const handlePhoneFilter = (e) => {
+    setIsPhoneFilter(!isPhoneFilter);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="App flex justify-center items-center h-screen sky-bg">
+      <header className="App-header max-w-screen-md">
+        <img
+          src="images/kevalalogo.png"
+          alt="kevalalogo"
+          className="h-20 mb-10 mx-auto grow hover:grow"
+        />
+        <h1 className="mt-1 text-4xl font-extrabold  sm:text-5xl sm:tracking-tight lg:text-6xl m-8 text-center text-slate-800">
+          Upload CSV to remove duplicates.
+        </h1>
+        <div className="mt-12 sm:mx-auto sm:w-full sm:max-w-md">
+          <div className="bg-white py-8 px-4 shadow sm:px-10 clay">
+            <form
+              data-testid="form-input"
+              className="space-y-7 p-2"
+              onSubmit={handleSubmit}
+            >
+              <div>
+                <label
+                  htmlFor="file"
+                  className="block text-sm font-medium text-gray-700"
+                ></label>
+                <div className="mt-1">
+                  <input
+                    className="block w-full text-sm text-slate-500
+                    file:mr-4 file:py-2 file:px-4
+                    file:rounded-full file:border-0
+                    file:text-sm file:font-semibold
+                    file:bg-sky-50 file:text-[#3898EC]
+                    hover:file:bg-sky-100 "
+                    data-testid="file-input"
+                    type="file"
+                    name="file"
+                    accept=".csv"
+                    required
+                    files={files}
+                    onChange={handleFile}
+                  />
+                </div>
+              </div>
+              <p>Choose filtering method(s) below:</p>
+
+              <div className="relative flex items-start">
+                <div className="flex items-center h-5">
+                  <input
+                    className="focus:ring-[#3898EC] h-4 w-4 border-gray-300 rounded-full"
+                    data-testid="email-input"
+                    name="email"
+                    type="checkbox"
+                    checked={isEmailFilter}
+                    onChange={handleEmailFilter}
+                  />
+                </div>
+                <div className="ml-3 text-sm">
+                  <label htmlFor="email" className="font-medium text-gray-700">
+                    Email
+                  </label>
+                  <p id="email-description" className="text-gray-500">
+                    Filter out rows with duplicate emails
+                  </p>
+                </div>
+              </div>
+
+              <div className="relative flex items-start">
+                <div className="flex items-center h-5">
+                  <input
+                    className="focus:ring-[#3898EC] h-4 w-4  border-gray-300 rounded-full"
+                    data-testid="phone-input"
+                    name="phone"
+                    type="checkbox"
+                    checked={isPhoneFilter}
+                    onChange={handlePhoneFilter}
+                  />
+                </div>
+                <div className="ml-3 text-sm">
+                  <label htmlFor="phone" className="font-medium text-gray-700">
+                    Phone
+                  </label>
+                  <p id="phone-description" className="text-gray-500">
+                    Filter out rows with duplicate phone numbers
+                  </p>
+                </div>
+              </div>
+
+              <input
+                className="inline-flex items-center mx-[210px] px-10 py-3 border border-transparent text-base font-medium rounded-full shadow-sm text-white bg-[#3898EC] hover:bg-blue-500 focus:outline-none focus:ring-2 focus:ring-offset-2"
+                type="submit"
+                value="Submit"
+                disabled={isFormDisabled}
+                data-testid="submit-button"
+              />
+            </form>
+          </div>
+        </div>
       </header>
     </div>
   );
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,172 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen, fireEvent, waitFor } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe("App component", () => {
+  it("renders without error", () => {
+    render(<App />);
+
+    const fileInput = screen.getByTestId("file-input");
+    expect(fileInput).toBeInTheDocument();
+
+    const emailInput = screen.getByTestId("email-input");
+    expect(emailInput).toBeInTheDocument();
+    expect(emailInput).not.toBeChecked();
+
+    const phoneInput = screen.getByTestId("phone-input");
+    expect(phoneInput).toBeInTheDocument();
+    expect(phoneInput).not.toBeChecked();
+
+    const submitButton = screen.getByTestId("submit-button");
+    expect(submitButton).toBeInTheDocument();
+    expect(submitButton).toBeDisabled();
+  });
+
+  it("enables submit button when file and filter type is selected", () => {
+    let mockCsv = new File([""], "mockFile.csv", { type: "text/csv" });
+    // const alertSpy = jest.spyOn(window, "alert");
+    render(<App />);
+    const submitButton = screen.getByTestId("submit-button");
+    const emailInput = screen.getByTestId("email-input");
+    const fileInput = screen.getByTestId("file-input");
+
+    fireEvent.change(fileInput, { target: { files: [mockCsv] } });
+    expect(fileInput.files[0]).toBe(mockCsv);
+    expect(fileInput.files[0].type).toBe("text/csv");
+
+    expect(submitButton).toBeDisabled();
+    fireEvent.click(emailInput);
+    expect(emailInput).toBeChecked();
+    expect(submitButton).toBeEnabled();
+  });
+  /////////
+  it("filters out by email", async () => {
+    const csvData = `Luffy,MonkeyD,captain@sunny.com,1112223333
+    Luffy,MonkeyD,pirate@sunny.com,1112223333
+    Sanji,Vinsmoke,pirate@sunny.com,9990008888`;
+    jest.spyOn(global, "File").mockImplementation(function () {
+      this.text = () => csvData;
+    });
+    let mockCsvFile = new File([csvData], "mockFile.csv", { type: "text/csv" });
+
+    const expectedString = `Luffy,MonkeyD,captain@sunny.com,1112223333
+    Luffy,MonkeyD,pirate@sunny.com,1112223333`;
+    const newCsvFile = encodeURI(
+      "data:text/csv;charset=utf-8," + expectedString
+    );
+
+    render(<App />);
+    const fileInput = screen.getByTestId("file-input");
+    const emailInput = screen.getByTestId("email-input");
+    const submitButton = screen.getByTestId("submit-button");
+    const windowSpy = jest.spyOn(window, "open");
+    windowSpy.mockImplementation(jest.fn());
+
+    fireEvent.change(fileInput, { target: { files: [mockCsvFile] } });
+    fireEvent.click(emailInput);
+    fireEvent.click(submitButton);
+    await waitFor(() => {
+      expect(windowSpy).toHaveBeenCalledWith(newCsvFile);
+    });
+  });
+
+  it("filters out by phone number", async () => {
+    const csvData = `Luffy,MonkeyD,captain@sunny.com,1112223333
+    Luffy,MonkeyD,pirate@sunny.com,1112223333
+    Sanji,Vinsmoke,pirate@sunny.com,9990008888`;
+    jest.spyOn(global, "File").mockImplementation(function () {
+      this.text = () => csvData;
+    });
+    let mockCsvFile = new File([csvData], "mockFile.csv", { type: "text/csv" });
+
+    const expectedString = `Luffy,MonkeyD,captain@sunny.com,1112223333
+    Sanji,Vinsmoke,pirate@sunny.com,9990008888`;
+    const newCsvFile = encodeURI(
+      "data:text/csv;charset=utf-8," + expectedString
+    );
+
+    render(<App />);
+    const fileInput = screen.getByTestId("file-input");
+    const phoneInput = screen.getByTestId("phone-input");
+    const submitButton = screen.getByTestId("submit-button");
+    const windowSpy = jest.spyOn(window, "open");
+    windowSpy.mockImplementation(jest.fn());
+
+    fireEvent.change(fileInput, { target: { files: [mockCsvFile] } });
+    fireEvent.click(phoneInput);
+    fireEvent.click(submitButton);
+    await waitFor(() => {
+      expect(windowSpy).toHaveBeenCalledWith(newCsvFile);
+    });
+  });
+
+  it("filters out by email and phone number", async () => {
+    const csvData = `Luffy,MonkeyD,captain@sunny.com,1112223333
+    Luffy,MonkeyD,pirate@sunny.com,1112223333
+    Sanji,Vinsmoke,pirate@sunny.com,9990008888
+    Chopper,TonyTony,pirate@sunny.com,2223334444`;
+    jest.spyOn(global, "File").mockImplementation(function () {
+      this.text = () => csvData;
+    });
+    let mockCsvFile = new File([csvData], "mockFile.csv", { type: "text/csv" });
+
+    const expectedString = `Luffy,MonkeyD,captain@sunny.com,1112223333
+    Sanji,Vinsmoke,pirate@sunny.com,9990008888`;
+    const newCsvFile = encodeURI(
+      "data:text/csv;charset=utf-8," + expectedString
+    );
+
+    render(<App />);
+    const fileInput = screen.getByTestId("file-input");
+    const phoneInput = screen.getByTestId("phone-input");
+    const emailInput = screen.getByTestId("email-input");
+    const submitButton = screen.getByTestId("submit-button");
+    const windowSpy = jest.spyOn(window, "open");
+    windowSpy.mockImplementation(jest.fn());
+
+    fireEvent.change(fileInput, { target: { files: [mockCsvFile] } });
+    fireEvent.click(emailInput);
+    fireEvent.click(phoneInput);
+    fireEvent.click(submitButton);
+    await waitFor(() => {
+      expect(windowSpy).toHaveBeenCalledWith(newCsvFile);
+    });
+  });
+
+  it("does not filter out blank entries as duplicates", async () => {
+    const csvData = `Luffy,MonkeyD,captain@sunny.com,1112223333
+    Luffy,MonkeyD,,
+    Sanji,Vinsmoke,,9990008888
+    Chopper,TonyTony,pirate@sunny.com,`;
+    jest.spyOn(global, "File").mockImplementation(function () {
+      this.text = () => csvData;
+    });
+    let mockCsvFile = new File([csvData], "mockFile.csv", { type: "text/csv" });
+
+    const expectedString = csvData;
+    const newCsvFile = encodeURI(
+      "data:text/csv;charset=utf-8," + expectedString
+    );
+
+    render(<App />);
+    const fileInput = screen.getByTestId("file-input");
+    const phoneInput = screen.getByTestId("phone-input");
+    const emailInput = screen.getByTestId("email-input");
+    const submitButton = screen.getByTestId("submit-button");
+    const windowSpy = jest.spyOn(window, "open");
+    windowSpy.mockImplementation(jest.fn());
+
+    fireEvent.change(fileInput, { target: { files: [mockCsvFile] } });
+    fireEvent.click(emailInput);
+    fireEvent.click(phoneInput);
+    fireEvent.click(submitButton);
+    await waitFor(() => {
+      expect(windowSpy).toHaveBeenCalledWith(newCsvFile);
+    });
+  });
+
+  /////////
+
+  ////////
 });
+
+// test for form upload
